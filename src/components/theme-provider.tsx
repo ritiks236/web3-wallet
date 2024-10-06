a29f74d1ffc20483822565b5237f56e231ec6b19
ritiks236 <ritiks236@gmail.com>
2024-10-06 14:40:08 +0530

    Added background

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,7 +3,13 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { BackgroundBeams } from "./ui/background-beams";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <BackgroundBeams />
+      {children}
+    </NextThemesProvider>
+  );
 }
