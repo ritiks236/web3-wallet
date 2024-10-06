4bf9ac127a70757c3c084f6baca7d07381778484
ritiks236 <ritiks236@gmail.com>
2024-10-06 11:53:37 +0530

    initial setup

---
+++ src/app/header.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { ModeToggle } from "@/components/mode-toggle";
+
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <ModeToggle />
+      </div>
+    </header>
+  );
+};
